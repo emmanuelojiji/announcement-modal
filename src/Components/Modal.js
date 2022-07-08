7310177dcf882e22d57b764538de3d2f3eccfc1e
emmanuelojiji <emmanuelojiji@hotmail.com>
2022-07-08 13:57:34 +0100

    Removed spacing between elements

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -5,6 +5,7 @@ const Modal = () => {
   return (
     <div className="modal-container">
       <div className="modal">
+          <div className="modal-header"></div>
         <div className="modal-content">
           <h3>Schedule nudges on your resources</h3>
           <p>
